5a047d22fefd7dbefe6dc846da3af1d48b5c4d35
Supreet <suprith.prakash.sp@gmail.com>
2020-07-01 23:39:22 +0530

    Modified component . added array

--- src/index.js
+++ src/index.js
@@ -8,13 +8,21 @@ import * as serviceWorker from './serviceWorker';
 import Customer from './Customer';
 
 const fname = "Supreet";
-const product = <Products id="1" name="mobile" cost="20000" quantity="20"></Products>
-const customer = <Customer Id="1" name="Tom" city="Bangalore" numbet="99888888888"></Customer>
+const product =
+    [
+        
+             < Products id = "1" name = "mobile" cost = "20000" quantity = "20" ></Products >
+        ,
+        <br/>,
+        < Products id="2" name="laptop" cost="7000" quantity="20" ></Products >
+    ]
+
+const customer = <Customer Id="1" name="Tom" city="Bangalore" number="99888888888"></Customer>
 ReactDOM.render(
    [ <Heading/>,
         <p>this is p tag</p>,
         <p> my first name is {fname}</p>,
-        product, customer],
+        product,<br/>, customer],
        
         
     
